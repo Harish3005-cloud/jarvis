95a174bed4ebd477eb44a1082fe21370a224ca35
Harish3005-cloud <eharish3005@gmail.com>
2025-01-27 20:30:54 +0530

    commit to tell the latest news

---
+++ feature_4.py
@@ -0,0 +1,43 @@
+import speech_recognition as sr 
+import datetime
+import subprocess
+import pywhatkit
+import pyttsx3
+import webbrowser
+import wikipedia
+import os
+import random
+
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 150)
+recognizer = sr.Recognizer()
+exit_flag = False
+
+
+
+with sr.Microphone() as source:
+        print("Clearing background noises...Please wait")
+        recognizer.adjust_for_ambient_noise(source, duration=0.5)
+        print('Ask me anything...')
+        try:
+            recordedaudio = recognizer.listen(source)
+            text = recognizer.recognize_google(recordedaudio, language='en_US')
+            text = text.lower()
+            print('Your message:', text)
+        except Exception as ex:
+            print("Could not recognize your voice. Error:", ex)
+            #return  # Exit early if no valid input
+def speak(text):
+    """Speak the given text using pyttsx3."""
+    engine.say(text)
+    try:
+        engine.runAndWait()
+    except RuntimeError:
+        pass  # Ignore runtime errors if the event loop is already running
+
+
+if 'news' in text:
+        speak("Fetching the latest news for you.")
+        webbrowser.open("https://news.google.com")
